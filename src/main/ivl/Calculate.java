16ab1943de064cd776862e7a7c0348bbd0518ad6
Arvind Swarnkar <soniarvind2004@gmail.com>
2016-06-14 18:57:15 +0530

    first commit

--- src/main/ivl/Calculate.java
+++ src/main/ivl/Calculate.java
@@ -2,7 +2,7 @@ package main.ivl;
 
 public class Calculate {
 	public int sum(int var1, int var2) {
-		System.out.println("Adding values: " + var1 + " + " + var2);
+		System.out.println("Adding values: " + var1 + " +  " + var2);
 		return var1 + var2;
 		
 		
